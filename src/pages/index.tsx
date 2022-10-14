c3bff88d2c0b21da0c4c292bab14f0e96b381508
JD_Rodrigues <jdevproject01@gmail.com>
2022-10-14 15:07:49 -0300

    Criação da seção de porfólio, com a lista de projetos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,21 +23,30 @@ const Home =  ({data}:IHomeProps) => {
   return (
     <div className={styles.home}>
       <header>
-        <div className={`${styles.cover} ${styles.cover__image}`}>
+        <div className={`${styles.cover} ${styles.cover__image} ${styles.container} ${styles.background__section}`}>
         </div>
-        <div className={`${styles.cover} ${styles.cover__title}`}>
+        <div className={`${styles.cover} ${styles.cover__title} ${styles.container}`}>
           <h1><span className={styles.first__name}>domingos</span>rodrigues</h1>
           <div className={styles.header__arrow__down}><Image alt='Clique aqui para ser direcionado para a próxima seção' src={arrowDown} /></div>
         </div>
       </header>
-      <h1>Portfólio</h1>
-      <ul>
-      { data && data.map((project:TProject)=>{
-        return(
-          <li key={project.name}><Link href={`/projects/${project.id}`}><h3>{project.name}</h3></Link></li> 
-        )
-      })}
-      </ul>
+      <main>
+        <section className={`${styles.section__portfolio__bg__img} ${styles.container} ${styles.background__section}`}>
+          
+          <div className={`${styles.section__portfolio__bg__gradient} ${styles.container} ${styles.background__section}`}>
+            <nav>
+              <h1>PORTFÓLIO</h1>
+              <ul className={styles.projects__list}>
+              { data && data.map((project:TProject)=>{
+                return(
+                  <li key={project.name}><Link href={`/projects/${project.id}`}><h3 className={styles.project__name}>{project.name}</h3></Link></li> 
+                )
+              })}
+              </ul>
+            </nav>
+          </div>
+        </section>
+      </main>
       
     </div>
   )
