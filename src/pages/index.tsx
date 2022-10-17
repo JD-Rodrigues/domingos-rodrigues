cac199e7a5dd7406c51df636ff32472edef505fb
JD_Rodrigues <jdevproject01@gmail.com>
2022-10-17 01:14:04 -0300

    Responsividade da página de projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,9 @@ import githubLogo from "../../public/images/logos/github-logo-thin.svg"
 import linkedinLogo from "../../public/images/logos/linkedin-logo-thin.svg"
 import instagramLogo from "../../public/images/logos/instagram-logo-thin.svg"
 import whatsappLogo from "../../public/images/logos/whatsapp-logo-thin.svg"
-import emailLogo from "../../public/images/logos/email-logo-thin.svg"
 import profilePic from "../../public/images/profile-pic.png"
+import  Head  from 'next/head'
+
 
 
 
@@ -27,101 +28,107 @@ export const getStaticProps = () => {
 const Home =  ({data}:IHomeProps) => {
 
   return (
-    <div className={styles.home}>
-      <header id='top' className={`${styles.cover} ${styles.cover__image} ${styles.container} ${styles.background__section}`} >
-        <div className={`${styles.cover} ${styles.cover__title} ${styles.container}`}>
-          <div>
-            <h1><span className={styles.first__name}>domingos</span>rodrigues</h1>
-            <p className={styles.career}>DESENVOLVEDOR<span className={styles.speciality}>FRONT-END</span></p>
-          </div>     
-          <div className={styles.arrow__wrapper}>
-            <a className={styles.header__arrow__down} href={"#portfolio__section"}><Image alt='Clique aqui para ser direcionado para a próxima seção' src={arrowDown} /></a>  
-          </div>            
-        </div>
-      </header>
-      <main>
-        <section className={`${styles.section__portfolio__bg__img} ${styles.background__section}`} id="portfolio__section">
-          
-          <div className={`${styles.section__portfolio__bg__gradient} ${styles.container} ${styles.background__section}`}>
-            <nav>
-              <h1 className={styles.title}>PORTFÓLIO</h1>
-              <ul className={styles.projects__list}>
-              { data && data.map((project:TProject)=>{
-                return(
-                  <li key={project.name}><Link href={`/projects/${project.id}`}><h3 className={styles.project__name}>{project.name}</h3></Link></li> 
-                )
-              })}
+    <>
+      <Head>
+        <title>Domingos Rodrigues - Desenvolvedor Front-end</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </Head>
+      <div className={styles.home}>
+        <header id='top' className={`${styles.cover} ${styles.cover__image} ${styles.container} ${styles.background__section}`} >
+          <div className={`${styles.cover} ${styles.cover__title} ${styles.container}`}>
+            <div>
+              <h1><span className={styles.first__name}>domingos</span>rodrigues</h1>
+              <p className={styles.career}>DESENVOLVEDOR<span className={styles.speciality}>FRONT-END</span></p>
+            </div>     
+            <div className={styles.arrow__wrapper}>
+              <a className={styles.header__arrow__down} href={"#portfolio__section"}><Image alt='Clique aqui para ser direcionado para a próxima seção' src={arrowDown} /></a>  
+            </div>            
+          </div>
+        </header>
+        <main>
+          <section className={`${styles.section__portfolio__bg__img} ${styles.background__section}`} id="portfolio__section">
+            
+            <div className={`${styles.section__portfolio__bg__gradient} ${styles.container} ${styles.background__section}`}>
+              <nav>
+                <h1 className={styles.title}>PORTFÓLIO</h1>
+                <ul className={styles.projects__list}>
+                { data && data.map((project:TProject)=>{
+                  return(
+                    <li key={project.name}><Link href={`/projects/${project.id}`}><h3 className={styles.project__name}>{project.name}</h3></Link></li> 
+                  )
+                })}
+                </ul>
+              </nav>
+            </div>
+            <div className={`${styles.wave} ${styles.wave__main__color__descending } ${styles.background__section}`}>
+            </div>
+          </section>
+          <section className={`${styles.container} ${styles.section__about__me}`}>
+            <div className={styles.about__me__text}>
+                <h2 className={styles.title}>SOBRE MIM</h2>
+                <p>
+                Eu sou Domingos Rodrigues, programador front-end, baiano, viciado em desenvolver soluções. Sou movido e orientado por desafios.
+                </p>
+                <br />
+                <p>
+                Trabalho com construção de interfaces e aplicações para a web utilizando tecnologias do ecossistema Javascript, prezando por um código limpo, performático e otimizado para os motores de busca.
+                </p>
+                <br />
+                <p>
+                Possuo habilidades de comunicação, aprendizado rápido e transmissão de informações, adquiridas em minha carreira anterior, como professor. Isso me possibilita contribuir com os demais profissionais, ajudando a compreender processos e o uso de tecnologias, reduzindo o tempo gasto com soluções que requerem técnicas ou abordagens utilizadas com pouca frequência pela equipe.
+                </p>
+            </div>
+            <div className={styles.profile__pic__wrapper} >
+              <div className={styles.profile__pic}>
+                <Image src={profilePic} />
+              </div>
+            </div>
+          </section>
+          <section className={`${styles.container} ${styles.section__techs}`}>
+            <div className={`${styles.wave} ${styles.wave__main__ascending } ${styles.background__section}`}>
+            </div>
+            <div className={styles.techs__text}>
+              <h2 className={styles.title}>TECNOLOGIAS</h2>
+              <p>
+                Tenho trabalhado com estas tecnologias. Além disso, estou sempre atento às ferramentas que o mercado está utilizando e incorporando-as ao meu trabalho.</p> 
+              <p>Aprendizado contínuo e evolução constante.</p>
+              <ul className={styles.techs__list}>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>Typescript</li>
+                <li>React</li>
+                <li>Next Js</li>
+                <li>Tailwind CSS</li>
+                <li>Jest</li>
+                <li>Git/Github</li>              
               </ul>
-            </nav>
-          </div>
-          <div className={`${styles.wave} ${styles.wave__main__color__descending } ${styles.background__section}`}>
-          </div>
-        </section>
-        <section className={`${styles.container} ${styles.section__about__me}`}>
-          <div className={styles.about__me__text}>
-              <h2 className={styles.title}>SOBRE MIM</h2>
-              <p>
-              Eu sou Domingos Rodrigues, programador front-end, baiano, viciado em desenvolver soluções. Sou movido e orientado por desafios.
-              </p>
-              <br />
-              <p>
-              Trabalho com construção de interfaces e aplicações para a web utilizando tecnologias do ecossistema Javascript, prezando por um código limpo, performático e otimizado para os motores de busca.
-              </p>
-              <br />
-              <p>
-              Possuo habilidades de comunicação, aprendizado rápido e transmissão de informações, adquiridas em minha carreira anterior, como professor. Isso me possibilita contribuir com os demais profissionais, ajudando a compreender processos e o uso de tecnologias, reduzindo o tempo gasto com soluções que requerem técnicas ou abordagens utilizadas com pouca frequência pela equipe.
-              </p>
-          </div>
-          <div className={styles.profile__pic__wrapper} >
-            <div className={styles.profile__pic}>
-              <Image src={profilePic} />
             </div>
-          </div>
-        </section>
-        <section className={`${styles.container} ${styles.section__techs}`}>
-          <div className={`${styles.wave} ${styles.wave__main__ascending } ${styles.background__section}`}>
-          </div>
-          <div className={styles.techs__text}>
-            <h2 className={styles.title}>TECNOLOGIAS</h2>
-            <p>
-              Tenho trabalhado com estas tecnologias. Além disso, estou sempre atento às ferramentas que o mercado está utilizando e incorporando-as ao meu trabalho.</p> 
-            <p>Aprendizado contínuo e evolução constante.</p>
-            <ul className={styles.techs__list}>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>Javascript</li>
-              <li>Typescript</li>
-              <li>React</li>
-              <li>Next Js</li>
-              <li>Tailwind CSS</li>
-              <li>Jest</li>
-              <li>Git/Github</li>              
-            </ul>
-          </div>
-        </section>
-      </main>
-      <footer >
-          <address className={`${styles.container} ${styles.section__networks}`}>
-            <h2 className={styles.title}>REDES E CONTATO</h2>
-            <div className={styles.footer__logos__wrapper}>
-              <div className={styles.footer__logo}>
-                <Image src={githubLogo} />
+          </section>
+        </main>
+        <footer >
+            <address className={`${styles.container} ${styles.section__networks}`}>
+              <h2 className={styles.title}>REDES E CONTATO</h2>
+              <div className={styles.footer__logos__wrapper}>
+                <div className={styles.footer__logo}>
+                  <Image src={githubLogo} />
+                </div>
+                <div className={styles.footer__logo}>
+                  <Image src={linkedinLogo} />
+                </div>
+                <div className={styles.footer__logo}>
+                  <Image src={instagramLogo} />
+                </div>
+                <div className={styles.footer__logo}>
+                  <Image src={whatsappLogo} />
+                </div> 
               </div>
-              <div className={styles.footer__logo}>
-                <Image src={linkedinLogo} />
-              </div>
-              <div className={styles.footer__logo}>
-                <Image src={instagramLogo} />
-              </div>
-              <div className={styles.footer__logo}>
-                <Image src={whatsappLogo} />
-              </div> 
-            </div>
-            <p>domingosrodrigues.dev@gmail.com</p>
-          </address>
-          <a href='#top' className="to__top"></a>
-        </footer>
-    </div>
+              <p>domingosrodrigues.dev@gmail.com</p>
+            </address>
+            <a href='#top' className="to__top"></a>
+          </footer>
+      </div>
+    </>
   )
 }
 
--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -3,6 +3,7 @@ import styles from "./[id].module.css"
 import { IGetStaticProps, IProjectProps, TProject } from "../../types"
 import data from "../../../public/database_projects"
 import Link from "next/link"
+import Head from "next/head"
 
 
 export const getStaticPaths = () => {
@@ -26,38 +27,47 @@ export const getStaticProps = ({ params }:IGetStaticProps) => {
 
 const Project =  ({project}:IProjectProps) => {
   return(
-    <div className={styles.project}>
-      <header className={styles.header} style={{backgroundImage:`url("${project.backdrop_path}")`}}>
-        <nav>
-          <Link href={"/"}><div className={styles.back__home}><span>⇽ Home</span></div></Link>
-        </nav>
-        <div className={styles.project__info}>
-          <h1 className={styles.project__name}>{project.name}</h1>
-          <p>{project.about}</p>
-          <p>{project.objective}</p>
-          <p>FEATURES:</p>
-          <ul className={styles.features__list}>
-            {project.features.map((feat:string)=><li key={feat}>{feat}</li>)}
+    <>
+      <Head>
+        <title>{project.name} - por Domingos Rodrigues</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </Head>
+      <div className={styles.project}>
+        <header className={styles.header} style={{backgroundImage:`url("${project.backdrop_path}")`}}>
+          <nav>
+            <Link href={"/"}><div className={styles.back__home}><span>⇽ Home</span></div></Link>
+          </nav>
+          <div className={styles.project__info}>
+            <h1 className={styles.project__name}>{project.name}</h1>
+            <p>{project.about}</p>
+            <p>{project.objective}</p>
+            <p>FEATURES:</p>
+            <ul className={styles.features__list}>
+              {project.features.map((feat:string)=><li key={feat}>{feat}</li>)}
+            </ul>
+            <p>{`© ${project.year} Domingos Rodrigues`}</p>
+            <a rel="noreferrer" target={"_blank"} className={styles.live} href={project.live}>Ver online</a>
+            <a href="#shots" ><div className={styles.go__next__section}></div></a>
+          </div>
+      </header>
+      <main>
+        <section id="shots" className={styles.screenshots}>
+          <img className={styles.demo} src={project.demo} />
+          <ul className={styles.shotlist}>
+          {project.screenshots.tablet && <img className={styles.shot__tablet} src={project.screenshots.tablet} />}
+            {project.screenshots.smartphone.map((shot:string)=>{
+              return(
+                <li className={styles.shot} key={shot}><img src={shot} /></li>
+              )
+            })}
           </ul>
-          <p>{`© ${project.year} Domingos Rodrigues`}</p>
-          <a rel="noreferrer" target={"_blank"} className={styles.live} href={project.live}>Ver online</a>
-          <a href="#shots" ><div className={styles.go__next__section}></div></a>
-        </div>
-    </header>
-    <main>
-      <section id="shots" className={styles.screenshots}>
-        <img className={styles.demo} src={project.demo} />
-        <ul className={styles.shotlist}>
-        {project.screenshots.tablet && <img className={styles.shot__tablet} src={project.screenshots.tablet} />}
-          {project.screenshots.smartphone.map((shot:string)=>{
-            return(
-              <li className={styles.shot} key={shot}><img src={shot} /></li>
-            )
-          })}
-        </ul>
-      </section>
-    </main>
-    </div>
+        </section>
+      </main>
+      <footer className={styles.footer}>
+        <p>© 2022 - Domingos Rodrigues</p>
+      </footer>
+      </div>
+    </>
     
 
     
